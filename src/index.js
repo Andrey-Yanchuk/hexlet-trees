c6fdc78cbbf37cd5745383860dd20ad612f331d9
Yanchuk Andrey <orpiy@mail.ru>
2025-01-20 22:39:37 +0700

    feat: add getFileSize and du implementation in src/index.js

--- src/index.js
+++ src/index.js
@@ -156,3 +156,30 @@ const treeGetHidden = mkdir("/", [
 ]);
 // console.log(getHiddenFilesCount(treeGetHidden));
 /*-----------------------------------------------------*/
+const treeDu = mkdir('/', [
+  mkdir('etc', [
+    mkdir('apache'),
+    mkdir('nginx', [
+      mkfile('nginx.conf', { size: 800 }),
+    ]),
+    mkdir('consul', [
+      mkfile('config.json', { size: 1200 }),
+      mkfile('data', { size: 8200 }),
+      mkfile('raft', { size: 80 }),
+    ]),
+  ]),
+  mkfile('hosts', { size: 3500 }),
+  mkfile('resolve', { size: 1000 }),
+]);
+const getFileSize = (tree) => {
+  const meta = getMeta(tree)
+  if (isFile(tree)) return meta.size;
+  const children = getChildren(tree);
+  return children.reduce((acc, child) => acc + getFileSize(child), 0);
+}
+export const du = (tree) => {
+  const children = getChildren(tree);
+  const result = children.map((child) => [getName(child), getFileSize(child)]);
+  return result.sort((a, b) => b[1] - a[1]);
+};
+// console.log(du(treeDu));
